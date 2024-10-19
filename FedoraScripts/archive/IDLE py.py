86b0d43e6d0b1182c94ba1156b8b3559bc1b8b7f
Mattias Münster <mattiasmun@gmail.com>
2024-10-19 10:05:51 +0200

    edit

---
+++ FedoraScripts/archive/IDLE py.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+cmds = """\
+import gmpy2 as gp
+from timeit import timeit
+ctx = gp.get_context()
+ctx.precision = 127
+ctx.allow_complex = True
+π = gp.const_pi()
+φ = 0.5 + gp.sqrt(1.25)
+ψ = gp.mpfr('1.465571231876768026656731225219939108025')
+π, φ, ψ
+"""
+exec(cmds)
