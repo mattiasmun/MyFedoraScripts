060a057c6c260a7dec5460116bc7547dc4f9724d
Mattias Münster <mattiasmun@gmail.com>
2024-10-20 14:30:35 +0200

    edit

--- FedoraScripts/archive/IDLE py.py
+++ FedoraScripts/archive/IDLE py.py
@@ -9,6 +9,8 @@ ctx.allow_complex = True
 π = gp.const_pi()
 φ = 0.5 + gp.sqrt(1.25)
 ψ = gp.mpfr('1.465571231876768026656731225219939108025')
-print(π, φ, ψ)\
+print(π, φ, ψ)
+_1 = gp.mpfr(1)
+_m1, _0, _2 = _1 * -1, _1 * 0, _1 * 2\
 """
 exec(cmds)
