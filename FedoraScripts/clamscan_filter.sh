8dc2809288893eb45d7297b3ae45457225c8c1a8
mattiasmun <mattiasmun@gmail.com>
2025-11-30 15:27:18 +0100

    edit

--- FedoraScripts/clamscan_filter.sh
+++ FedoraScripts/clamscan_filter.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 # Detta skript fungerar som en mellanhand mellan FiltaQuilla och ClamAV.
-# Det anropar clamscan och returnerar en statuskod.
+# Det skannar en FIXXAD SÖKVÄG där Thunderbird/FiltaQuilla sparar bilagorna.
 
 # --------------------------
 # VARIABLER
 # --------------------------
-# Sökväg till den mapp FiltaQuilla skickar (med bilagor)
-BILAGA_DIR="$1"
-# Sökväg till din karantänmapp (använder $HOME för portabilitet)
+# Sökväg till den mapp Thunderbird/FiltaQuilla sparar bilagorna.
+SPARAD_BILAGA_DIR="$HOME/.clamtk/attachment"
+
+# Sökväg till din karantänmapp (där infekterade filer flyttas)
 KARANTAN_DIR="$HOME/.clamtk/quarantine" 
+
 # Sökväg till Clamscan binärfil
 CLAMSCAN_BIN="/usr/bin/clamscan"
 
-# Kontrollera att sökvägen till bilagan skickades
-if [ -z "$BILAGA_DIR" ]; then
-    exit 0 # Ingen sökväg skickades, avsluta som rent
-fi
-
 # --------------------------
-# 1. KÖR CLAMSCAN
+# 1. KÖR CLAMSCAN MOT DEN FIXA SÖKVÄGEN
 # --------------------------
 # Argument:
 # --no-summary: Håll utdata ren
-# -r: Skanna rekursivt (för bilagor inuti bilagor/temporära mappar)
+# -r: Skanna rekursivt
 # --move: Flytta den infekterade filen direkt till karantän
-"$CLAMSCAN_BIN" --no-summary -r --move="$KARANTAN_DIR" "$BILAGA_DIR"
+# Sökväg: Vi skannar den sparade mappen direkt.
+"$CLAMSCAN_BIN" --no-summary -r --move="$KARANTAN_DIR" "$SPARAD_BILAGA_DIR"
 
 # Fånga exit-koden från clamscan.
 CLAMSCAN_STATUS=$?
@@ -34,15 +32,18 @@ CLAMSCAN_STATUS=$?
 # --------------------------
 
 if [ "$CLAMSCAN_STATUS" -eq 0 ]; then
-    # Exit code 0: Rent. Låt meddelandet passera.
+    # Exit code 0: Rent. Inga hot hittades i den sparade mappen.
+    # Rensa den temporära sparade bilagemappen.
+    rm -rf "$SPARAD_BILAGA_DIR"/*
     exit 0
 elif [ "$CLAMSCAN_STATUS" -eq 1 ]; then
-    # Exit code 1: Virushot hittades OCH flyttades.
-    # Vi returnerar en icke-noll-kod för att signalera att något hände.
-    # FiltaQuilla kommer att tolka detta som "Lyckades Inte" (Failed/Not Succeeded).
-    # Vi kan nu använda en sekundär åtgärd i filtret, t.ex. lägga till en tagg.
+    # Exit code 1: Virushot hittades OCH flyttades till karantän.
+    # Även om hotet är borta, returnerar vi 10 för att indikera att något HÄNDE.
+    # Rensa den nu tomma sparade bilagemappen.
+    rm -rf "$SPARAD_BILAGA_DIR"/*
     exit 10
 else
-    # Andra fel (t.ex. fil hittades inte, databasfel)
+    # Andra fel (t.ex. databasfel, sökvägsproblem)
+    # Låt mappen vara kvar för felsökning.
     exit 99
 fi
