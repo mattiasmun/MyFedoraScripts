e160c57c61bd79535495b161f19956dd21fa3761
mattiasmun <mattiasmun@gmail.com>
2025-11-30 15:29:26 +0100

    edit

--- FedoraScripts/clamscan_filter.sh
+++ FedoraScripts/clamscan_filter.sh
@@ -15,6 +15,12 @@ KARANTAN_DIR="$HOME/.clamtk/quarantine"
 CLAMSCAN_BIN="/usr/bin/clamscan"
 
 # --------------------------
+# 0. FÖRBEREDELSER
+# --------------------------
+# Skapa karantänmappen om den inte redan finns, för att undvika fel i clamscan.
+mkdir -p "$KARANTAN_DIR"
+
+# --------------------------
 # 1. KÖR CLAMSCAN MOT DEN FIXA SÖKVÄGEN
 # --------------------------
 # Argument:
@@ -28,22 +34,22 @@ CLAMSCAN_BIN="/usr/bin/clamscan"
 CLAMSCAN_STATUS=$?
 
 # --------------------------
-# 2. HANTERA RESULTAT
+# 2. HANTERA RESULTAT & RENSNING
 # --------------------------
 
 if [ "$CLAMSCAN_STATUS" -eq 0 ]; then
-    # Exit code 0: Rent. Inga hot hittades i den sparade mappen.
+    # Exit code 0: Rent. Inga hot hittades.
     # Rensa den temporära sparade bilagemappen.
     rm -rf "$SPARAD_BILAGA_DIR"/*
     exit 0
 elif [ "$CLAMSCAN_STATUS" -eq 1 ]; then
     # Exit code 1: Virushot hittades OCH flyttades till karantän.
-    # Även om hotet är borta, returnerar vi 10 för att indikera att något HÄNDE.
+    # Returnerar 10 för att Thunderbird ska fånga att en åtgärd utfördes.
     # Rensa den nu tomma sparade bilagemappen.
     rm -rf "$SPARAD_BILAGA_DIR"/*
     exit 10
 else
-    # Andra fel (t.ex. databasfel, sökvägsproblem)
-    # Låt mappen vara kvar för felsökning.
+    # Andra fel (t.ex. databasfel, sökvägsproblem).
+    # Låt mappen vara kvar för felsökning vid fel.
     exit 99
 fi
