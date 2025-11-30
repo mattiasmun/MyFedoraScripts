2129d4797676a97fc67f9a642c8dd1636e8a0225
mattiasmun <mattiasmun@gmail.com>
2025-11-30 16:01:22 +0100

    edit

--- FedoraScripts/clamscan_filter.sh
+++ FedoraScripts/clamscan_filter.sh
@@ -11,8 +11,8 @@ SPARAD_BILAGA_DIR="$HOME/.clamtk/attachment"
 # Sökväg till din karantänmapp (där infekterade filer flyttas)
 KARANTAN_DIR="$HOME/.clamtk/quarantine" 
 
-# Sökväg till Clamscan binärfil
-CLAMSCAN_BIN="/usr/bin/clamscan"
+# Byt till clamdscan för snabbare skanning via daemonen.
+CLAMSCAN_BIN="/usr/bin/clamdscan"
 
 # Loggfilens sökväg (vi lägger den i .clamtk/log/)
 LOGG_FIL="$HOME/.clamtk/log/clamscan_filter.log"
@@ -20,37 +20,42 @@ LOGG_FIL="$HOME/.clamtk/log/clamscan_filter.log"
 # --------------------------
 # 0. FÖRBEREDELSER
 # --------------------------
+# Tvinga Bash att starta i hemmakatalogen för att säkra att $HOME är korrekt.
+cd "$HOME"
+
 # Skapa karantän- och loggmappar om de inte redan finns.
 mkdir -p "$KARANTAN_DIR"
 mkdir -p "$HOME/.clamtk/log"
 
-# Lägg till tidsstämpel i loggen
+# Lägg till tidsstämpel och miljövärden i loggen
 echo "--- Skanning startad: $(date) ---" >> "$LOGG_FIL"
+echo "VERKTYG: clamdscan" >> "$LOGG_FIL"
+echo "Kontrollerad \$HOME: $HOME" >> "$LOGG_FIL"
+echo "Skanningskatalog: $SPARAD_BILAGA_DIR" >> "$LOGG_FIL"
+echo "Karantänkatalog: $KARANTAN_DIR" >> "$LOGG_FIL"
+
 
 # --------------------------
 # 0.5. KONTROLLERA INNEHÅLL
 # --------------------------
 # Säkerställ att om det inte finns några filer i mappen, avslutas skriptet.
-# 'shopt -s nullglob' ser till att '*.+' blir en tom sträng om det inte finns matchningar.
 shopt -s nullglob
 files=("$SPARAD_BILAGA_DIR"/*)
 shopt -u nullglob
 
 if [ ${#files[@]} -eq 0 ]; then
-    echo "Skanning klar: Ingen bilaga hittades i mappen. Avslutas." >> "$LOGG_FIL"
+    echo "Skanning klar: Ingen bilaga hittades i mappen. Avslutas. (Exit Code 0)" >> "$LOGG_FIL"
     exit 0
 fi
 
 # --------------------------
-# 1. KÖR CLAMSCAN MED LOGGNING
+# 1. KÖR CLAMDSCA MED FLYTT TILL KARANTÄN
 # --------------------------
-# Argument:
+# --move: Flyttar infekterade filer till karantän (detta är clamdscans inbyggda flagga)
 # -r: Skanna rekursivt
-# --move: Flytta den infekterade filen direkt till karantän
-# --log: Skriver alla meddelanden till loggfilen
-"$CLAMSCAN_BIN" -r --move="$KARANTAN_DIR" --log="$LOGG_FIL" "$SPARAD_BILAGA_DIR"
+"$CLAMSCAN_BIN" --move="$KARANTAN_DIR" -r --no-summary "$SPARAD_BILAGA_DIR" >> "$LOGG_FIL" 2>&1
 
-# Fånga exit-koden från clamscan.
+# Fånga exit-koden från clamdscan.
 CLAMSCAN_STATUS=$?
 
 # --------------------------
@@ -59,19 +64,20 @@ CLAMSCAN_STATUS=$?
 
 if [ "$CLAMSCAN_STATUS" -eq 0 ]; then
     # Exit code 0: Rent. Inga hot hittades.
-    echo "Skanning klar: Rent." >> "$LOGG_FIL"
+    echo "Skanning klar: Rent. (Exit Code 0)" >> "$LOGG_FIL"
     # Rensa den temporära sparade bilagemappen.
     rm -rf "$SPARAD_BILAGA_DIR"/*
     exit 0
 elif [ "$CLAMSCAN_STATUS" -eq 1 ]; then
     # Exit code 1: Virushot hittades OCH flyttades till karantän.
-    echo "Skanning klar: VIRUS HITTAT och flyttat till karantän: $KARANTAN_DIR" >> "$LOGG_FIL"
+    echo "Skanning klar: VIRUS HITTAT och flyttat till karantän. (Exit Code 1)" >> "$LOGG_FIL"
     # Rensa den nu tomma sparade bilagemappen.
     rm -rf "$SPARAD_BILAGA_DIR"/*
-    exit 10
+    exit 10 # Använder 10 för att indikera VIRUS
 else
-    # Andra fel.
-    echo "Skanning klar: FEL UPPSTOD (Exit Code $CLAMSCAN_STATUS). Kontrollera ClamAV-databasen." >> "$LOGG_FIL"
+    # Andra fel. Logga felkoden för diagnostik.
+    echo "Skanning klar: FEL UPPSTOD (Exit Code $CLAMSCAN_STATUS)." >> "$LOGG_FIL"
+    echo "Felet kan bero på: clamd@scan.service är inte igång, nätverksfel eller att clamdscan inte hittade något att skanna." >> "$LOGG_FIL"
     # Låt mappen vara kvar för felsökning vid fel.
     exit 99
 fi
