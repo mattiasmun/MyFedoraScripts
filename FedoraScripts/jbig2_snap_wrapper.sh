9905390560314ab41d1723fb62d4d3a01375488d
mattiasmun <mattiasmun@gmail.com>
2025-12-16 01:06:17 +0100

    edit

--- FedoraScripts/jbig2_snap_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Filnamn: jbig2_snap_wrapper.sh
-# Denna wrapper kör Snap-installerade jbig2enc och returnerar ENDAST relevant utdata.
-
-# Anropar den riktiga binären med alla argument ($@)
-# och hoppar över den initiala Snapd-diagnostik (som skrivs till stderr).
-/var/lib/snapd/snap/bin/jbig2enc "$@" 2>/dev/null
