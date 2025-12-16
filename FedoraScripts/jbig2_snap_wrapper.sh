4d9f03abe679936754b808c8feb906d73cd5557e
mattiasmun <mattiasmun@gmail.com>
2025-12-16 01:44:09 +0100

    edit

--- FedoraScripts/jbig2_snap_wrapper.sh
+++ FedoraScripts/jbig2_snap_wrapper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Filnamn: jbig2_snap_wrapper.sh
-# Fixar 'liblept.so.5' felet genom att lägga till Snaps bibliotekssökväg.
+# Fixar biblioteksfelen genom att lägga till Snaps bibliotekssökväg.
 
-# 1. Definiera den korrekta bibliotekssökvägen inuti Snap-paketet
+# 1. Definiera de nödvändiga bibliotekssökvägarna:
 SNAP_LIB_PATH="/var/lib/snapd/snap/jbig2enc/44/usr/lib"
+# Anta att PNG-biblioteket ligger här (justera sökvägen vid behov):
+SNAP_PNG_PATH="/var/lib/snapd/snap/jbig2enc/44/usr/lib/x86_64-linux-gnu" 
 
 # 2. Uppdatera LD_LIBRARY_PATH FÖR DETTA KOMMANDO
-# Lägger till den nya sökvägen i början av befintlig LD_LIBRARY_PATH.
-# Kör sedan den riktiga jbig2enc binären.
-LD_LIBRARY_PATH="$SNAP_LIB_PATH:$LD_LIBRARY_PATH" exec /snap/jbig2enc/44/bin/jbig2 "$@"
+# Lägger till båda sökvägarna i början av befintlig LD_LIBRARY_PATH.
+LD_LIBRARY_PATH="$SNAP_LIB_PATH:$SNAP_PNG_PATH:$LD_LIBRARY_PATH" exec /snap/jbig2enc/44/bin/jbig2 "$@"
