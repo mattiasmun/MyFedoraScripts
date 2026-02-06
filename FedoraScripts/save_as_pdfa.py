2ac9c08b9571449d39e6c512bc9df3c9f9d4a705
mattiasmun <mattiasmun@gmail.com>
2026-02-06 15:50:24 +0100

    edit

--- FedoraScripts/save_as_pdfa.py
+++ FedoraScripts/save_as_pdfa.py
@@ -5,17 +5,18 @@ import sys
 def convert_to_pdfa(input_file, output_file):
     # Kommando för att tvinga konvertering till PDF/A-2b
     gs_command = [
-        "gswin64c", "-dPDFA", "-dBATCH", "-dNOPAUSE",
+        "gswin64c", "-dPDFA=3", "-dBATCH", "-dNOPAUSE",
         "-sColorConversionStrategy=UseDeviceIndependentColor",
         "-sDEVICE=pdfwrite",
         "-dPDFACompatibilityPolicy=1",
         f"-sOutputFile={output_file}",
+        "PDFA_def.ps",
         input_file
     ]
 
     try:
         subprocess.run(gs_command, check=True)
-        print(f"Konvertering klar: {output_file}")
+        print(f"Konvertering till PDF/A-3b klar: {output_file}")
     except Exception as e:
         print(f"Fel vid konvertering: {e}")
 
